c0a96ef9b8a1f7ee2cf839b26771b318d42f54b0
Krishab Bashyal <krishab2002@gmail.com>
2022-05-16 21:48:26 -0500

    Made changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
+      <h1>
+        Welcome, from here you can take a look at all of your previous expenses.
+      </h1>
       <ExpenseItem />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2020, 6, 5);
+  const expenseTitle = "6800XT Graphics Card";
+  const expensePrice = 767.34;
+
   return (
-    <div>
-      <div>June 14th, 2022</div>
-      <div>
-        <h2>New GPU</h2>
-        <div>$767.45</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
