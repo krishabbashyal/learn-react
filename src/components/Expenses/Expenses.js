eeaed7d760b8eb1a9e4133ba7c326c0b4a1820c8
Krishab Bashyal <krishab2002@gmail.com>
2022-05-18 13:37:42 -0500

    created components that work with the state hook

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,21 +6,25 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
+        id={props.items[0].id}
         title={props.items[0].title}
         price={props.items[0].price}
         date={props.items[0].date}
       />
       <ExpenseItem
+        id={props.items[1].id}
         title={props.items[1].title}
         price={props.items[1].price}
         date={props.items[1].date}
       />
       <ExpenseItem
+        id={props.items[2].id}
         title={props.items[2].title}
         price={props.items[2].price}
         date={props.items[2].date}
       />
       <ExpenseItem
+        id={props.items[3].id}
         title={props.items[3].title}
         price={props.items[3].price}
         date={props.items[3].date}
