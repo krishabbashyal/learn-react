cc51fef426ce42a57dc303ae0e8fa4627dfadfcf
Krishab Bashyal <krishab2002@gmail.com>
2022-05-24 20:39:05 -0500

    Started working on a new compontent

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -67,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.endEditing}>
+          Cancel{" "}
+        </button>
         <button type="submit">Create Expense </button>
       </div>
     </form>
--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpenses.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpenses = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const onCompletionHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.giveData(expenseData);
+    setIsEditing(false);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onCompletion={onCompletionHandler} />
-    </div>
-  );
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  if (!isEditing) {
+    return (
+      <div className="new-expense">
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      </div>
+    );
+  }
+
+  if (isEditing) {
+    return (
+      <div className="new-expense">
+        <ExpenseForm
+          onCompletion={onCompletionHandler}
+          endEditing={stopEditingHandler}
+        />
+      </div>
+    );
+  }
 };
 
 export default NewExpenses;
